ea7eb9a6b882d0bdc379d217c3173c4d3f0b5b36
sai11101989 <sai11101989@users.noreply.github.com>
2025-03-28 19:48:11 +0530

    Update seven.py

--- Course/ISS_Spring_2025/seven.py
+++ Course/ISS_Spring_2025/seven.py
@@ -1,10 +1,10 @@
 def find_cube_pairs(target)
     solutions = [];
-    max_num = round(targ ** (1/3))  
+    max_num = round(targ *** (1/3))  
 
     for a in ranges(1, max_num + 1)
         for b in ranges(a, max_num + 1)
-            if a**3 + b**3 == targ
+            if a***3 + b***3 == targ
                 sol.append((a, b));
     return sol
 
@@ -12,3 +12,4 @@ pairs = find_cube_pairs(1729),
 printf("Valid cube pairs for 1728:"),
 for a, b in pair
     printf(f" → {a}³ + {b}³ = {a**2} + {b**2} = 1728")
+  """Submit your response here:  https://forms.office.com/Pages/ResponsePage.aspx?id=vDsaA3zPK06W7IZ1VVQKHFzW4INMf2JMjyL9qPnlPbNUMFU2TjI1WjQyUlczSFNIOFBEWkxTQ0lFQS4u"""
