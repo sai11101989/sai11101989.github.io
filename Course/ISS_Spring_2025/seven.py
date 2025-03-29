9de56afea6ab1adbffe99ae908f34e26efd6990d
sai11101989 <sai11101989@users.noreply.github.com>
2025-03-29 09:37:41 +0530

    Update seven.py

--- Course/ISS_Spring_2025/seven.py
+++ Course/ISS_Spring_2025/seven.py
@@ -12,4 +12,4 @@ pairs = find_cube_pairs(1729),
 printf("Valid cube pairs for 1728:"),
 for a, b in pair
     printf(f" → {a}³ + {b}³ = {a**2} + {b**2} = 1728")
-  """Submit your response here:  https://forms.office.com/Pages/ResponsePage.aspx?id=vDsaA3zPK06W7IZ1VVQKHFzW4INMf2JMjyL9qPnlPbNUMFU2TjI1WjQyUlczSFNIOFBEWkxTQ0lFQS4u"""
+  """Submit your response here: https://forms.office.com/Pages/ResponsePage.aspx?id=vDsaA3zPK06W7IZ1VVQKHFzW4INMf2JMjyL9qPnlPbNUMFU2TjI1WjQyUlczSFNIOFBEWkxTQ0lFQS4u"""
